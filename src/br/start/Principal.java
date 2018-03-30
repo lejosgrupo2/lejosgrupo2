89cc33b55e70944fc8c98917cc141660693291c3
lejosgrupo2 <37460599+lejosgrupo2@users.noreply.github.com>
2018-03-29 21:08:21 -0300

    lejo
    detecção de cor vermelha

--- src/br/start/Principal.java
+++ src/br/start/Principal.java
@@ -1,5 +1,6 @@
 package br.start;
 
+import br.tools.Router;
 import lejos.hardware.Button;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.Motor;
@@ -12,27 +13,18 @@ public class Principal {
 		
 		br.tools.ColorSensor Cs = new br.tools.ColorSensor();
 		
-		Cs.start();
-
-		lejos.hardware.sensor.EV3TouchSensor toq;
-		toq = new lejos.hardware.sensor.EV3TouchSensor(lejos.hardware.port.SensorPort.S1);
-		br.tools.SimpleTouch touch=new br.tools.SimpleTouch(toq);
-		if(Cs.getColor()==3){		
-			while (!touch.isPressed()) {
-				System.out.println("DESTINO OK!!");
-				System.out.println("Aperte o sensor de toque....\n");				
-				lejos.utility.Delay.msDelay(1000);
-			}
-		}else{
-			while (!touch.isPressed()) {
-				System.out.println("NAO ENCONTREI");
-				System.out.println(" O DESTINO!");
-				System.out.println("Aperte o sensor de toque....\n");				
-				lejos.utility.Delay.msDelay(1000);
-			}
-		}
+		Cs.start(); //liga sensor
 		
+		//lejos.hardware.sensor.EV3TouchSensor toq;
+		//toq = new lejos.hardware.sensor.EV3TouchSensor(lejos.hardware.port.SensorPort.S1);
+		//br.tools.SimpleTouch touch=new br.tools.SimpleTouch(toq);
+		Router rt = new Router();
+		while (Cs.getColor()!=0) {
+			rt.pathMove();
+			
+		}
 		Cs.TurnOff();
+		
 		
 	}
 
--- src/br/tools/Router.java
+++ src/br/tools/Router.java
@@ -5,6 +5,9 @@ public class Router {
 	public void path1(){
 		new br.tools.Truck().forward(340, 80);	
 	}
+	public void pathMove() {
+		new br.tools.Truck().forward(50, 80);	
+	}
 	
 	public void stop(){
 		new br.tools.Truck().stop();	
